077401589ae13409895ca573e340be237a1f3e83
informatica64-tools <64447674+informatica64-tools@users.noreply.github.com>
2020-11-13 01:08:18 +0100

    Create tools.sh

---
+++ tools.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+# Upgrade repositories
+sudo apt-get update
+
+# Install tools for my pentests
+sudo apt-get install -y curl nikto hydra sqlmap nmap hashcat netcat john yersinia aircrack-ng ssh vsftpd wireshark snort cadaver atftpd
+
+# Some tools neccesaries
+sudo apt-get install -y postgresql apache2 php
+
+# Spoofing tools
+sudo apt-get install -y dsniff exiftool net-tools ettercap-graphical steghide binwalk smbclient dnsrecon recon-ng
+
+# Kali Linux repositories
+sudo apt-get install gnupg -y
+sudo sh -c "echo 'deb https://http.kali.org/kali kali-rolling main non-free contrib' > /etc/apt/sources.list.d/kali.list"
+sudo apt-get update -y
+wget 'https://archive.kali.org/archive-key.asc'
+sudo apt-key add archive-key.asc
+sudo apt-get update -y
+sudo sh -c "echo 'Package: *'>/etc/apt/preferences.d/kali.pref; echo 'Pin: release a=kali-rolling'>>/etc/apt/preferences.d/kali.pref; echo 'Pin-Priority: 50'>>/etc/apt/preferences.d/kali.pref"
+sudo apt-get update -y
+
+sudo apt-get install exploitdb -y
+
+sudo apt autoremove && sudo apt-get update -y
+sudo rm arch*
+
