f6999e35606c4573c524760304ecc6c056fb2729
informatica64-tools <64447674+informatica64-tools@users.noreply.github.com>
2020-12-04 16:31:52 +0100

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -27,3 +27,5 @@ sudo apt-get install exploitdb -y
 sudo apt autoremove && sudo apt-get update -y
 sudo rm arch*
 
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && chmod 755 msfinstall && ./msfinstall
+rm msfinstall
